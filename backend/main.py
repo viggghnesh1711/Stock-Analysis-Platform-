7cd8bf19998fb135809f5f427b36f67c69eec857
straightcounter <sunnykachare602@gmail.com>
2026-04-04 15:59:27 +0530

    sdf

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://stockly-mauve.vercel.app"
+    ],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
